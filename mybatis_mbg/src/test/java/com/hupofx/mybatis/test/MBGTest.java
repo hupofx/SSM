2cb340ab04ed8140b832d60317b16e53e5261b9b
zdz <1159364181@qq.com>
2022-08-30 03:25:57 +0800

    mybatis-generator

---
+++ mybatis_mbg/src/test/java/com/hupofx/mybatis/test/MBGTest.java
@@ -0,0 +1,39 @@
+package com.hupofx.mybatis.test;
+
+import com.hupofx.mybatis.mapper.EmpMapper;
+import com.hupofx.mybatis.pojo.Emp;
+import com.hupofx.mybatis.utils.SqlSessionUtil;
+import org.apache.ibatis.session.SqlSession;
+import org.junit.Test;
+
+/**
+ * @author zdz
+ * @version V1.0
+ * @Package com.hupofx.mybatis.test
+ * @date 2022/8/30 3:09
+ */
+public class MBGTest {
+
+    @Test
+    public void testMBG() {
+        SqlSession sqlSession = SqlSessionUtil.getSqlSession();
+        EmpMapper mapper = sqlSession.getMapper(EmpMapper.class);
+        //根据id查询数据
+        /*Emp emp = mapper.selectByPrimaryKey(1);
+        System.out.println(emp);*/
+        //查询所有数据
+        /*List<Emp> list = mapper.selectByExample(null);
+        list.forEach(System.out::println);*/
+        //根据条件查询数据
+        /*EmpExample example = new EmpExample();
+        example.createCriteria().andEmpNameEqualTo("张三").andAgeGreaterThanOrEqualTo(20);
+        example.or().andGenderEqualTo("男");
+        List<Emp> list = mapper.selectByExample(example);
+        list.forEach(System.out::println);*/
+        Emp emp = new Emp(1,"小黑",null,"女");
+        //测试普通修改功能
+        //mapper.updateByPrimaryKey(emp);
+        //测试选择性修改
+        mapper.updateByPrimaryKeySelective(emp);
+    }
+}
